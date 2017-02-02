d18c77908e00c05ac4d6d8289a62f9a856d665d5
Pocket of Weird <hartzlernathan@gmail.com>
2017-02-02 16:39:01 -0600

    Styled login view

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import RaisedButton from 'material-ui/RaisedButton'
+import LockOutlineIcon from 'material-ui/svg-icons/action/lock-outline'
 import { login } from '../actions'
 
 
+const styles = {
+  height: '100%',
+  textAlign: 'center',
+  marginTop: '75%',
+  fontFamily: 'Arial, Helvetica, sans-serif'
+}
+
 const LoginView = (props) => (
-  <div onClick={props.handleLogin}>
-    Login
+  <div style={styles}>
+    <h3>Chord Goober</h3>
+    <RaisedButton
+      label='Sign In with Google'
+      icon={<LockOutlineIcon />}
+      onTouchTap={props.handleLogin}
+      primary={true}
+    />
   </div>
 )
 
